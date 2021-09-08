079d3ea621470494a047662bf94282efaaf22cc7
alanTBST <53797853+alanTBST@users.noreply.github.com>
2021-09-08 10:17:41 +0200

    docstrings and formatting

--- stoptimes.py
+++ stoptimes.py
@@ -25,10 +25,31 @@ T_STOPS_TIMES = Dict[int, Dict[int, Dict[str, Union[str, int, float]]]]
 
 DB_SIZE = 1 * 1024 * 1024 * 1024
 
-def read_stop_times(filepath: Path)-> T_STOPS_TIMES:
+def read_stop_times(stoptimes_filepath: Path)-> T_STOPS_TIMES:
+    """Load the stop_times.txt file, process it and convert it
+    to a dictionary
 
-    df = pd.read_csv(filepath, low_memory=False)
+    :param stoptimes_filepath: path to the stop_times.txt file
+    :type stoptimes_filepath: Path
+    :return: A dictionary of stoptimes of the form
+        {tripnum: {
+            stopsequencenum:{
+                'arrival_time': time,
+                'departure_time': time,
+                'stop_id': stopid,
+                'pickup_type': 1/0,
+                'drop_off_type':, 1/0
+                'stop_headsign': ''
+            }
+        }
+    }
+    :rtype: T_STOPS_TIMES
+    """
+
+    df = pd.read_csv(stoptimes_filepath, low_memory=False)
     df = df.fillna('0')
+
+    # this deals with older format gtfs from Rejseplan
     if not 'int' in df.loc[:, 'stop_id'].dtype.name:
         df.loc[:, 'stop_id'] = \
             df.loc[:, 'stop_id'].astype(str).str.strip('G').astype(int)
@@ -45,6 +66,14 @@ def write_stops_times_to_archive(
         stop_times: T_STOPS_TIMES,
         dates: str
         ) -> None:
+    """Write the stop_times data to the archive
+    This data set is placed in an lmdb key-value store
+
+    :param stop_times: a stop_times dictionary
+    :type stop_times: T_STOPS_TIMES
+    :param dates: a daterange string
+    :type dates: str
+    """
 
     archive_loc = str(Path(ARCHIVE_DIR, dates))
 
@@ -60,10 +89,12 @@ def write_stops_times_to_archive(
 
 
 def check_stop_times() -> None:
+    """
+    Read the new stoptimes data and write it to the archive
+    """
 
-    new_data = Path(TEMP_DIR, 'stop_times.txt')
+    new_data = TEMP_DIR / 'stop_times.txt'
+
     new_dates = find_date_range(TEMP_DIR)
     new_stop_times = read_stop_times(new_data)
     write_stops_times_to_archive(new_stop_times, new_dates)
-
-    return
